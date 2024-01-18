2fc5228425a8c398f0c0a1c20cddff745fdb9c88
oskarvedel <oskar.vedel@gmail.com>
2024-01-18 14:37:50 +0100

    depotrum page query

--- tdp-configs-plugin.php
+++ tdp-configs-plugin.php
@@ -5,6 +5,17 @@
  * Version: 1.0
  */
 
+
+// Remove dashicons in frontend for unauthenticated users
+add_action('wp_enqueue_scripts', 'bs_dequeue_dashicons');
+function bs_dequeue_dashicons()
+{
+    if (!is_user_logged_in()) {
+        wp_deregister_style('dashicons');
+    }
+}
+
+
 function modify_archive_query($query)
 {
     // Check if we are on the front end and if the main query is being modified
@@ -41,11 +52,26 @@ function modify_archive_query($query)
 add_action('pre_get_posts', 'modify_archive_query', 1);
 
 
-// Remove dashicons in frontend for unauthenticated users
-add_action('wp_enqueue_scripts', 'bs_dequeue_dashicons');
-function bs_dequeue_dashicons()
-{
-    if (!is_user_logged_in()) {
-        wp_deregister_style('dashicons');
+//create custom query for "depotrum" page
+add_action('elementor/query/depotrum_page_query', function ($query) {
+    // Check if we are on the front end and if the main query is being modified
+    if (!is_admin() && $query->is_main_query()) {
+        $geolocation_id = 29783; //set geolocation id to Denmark
+
+        $gd_place_list_combined = get_post_meta($geolocation_id, 'archive_gd_place_list', false);
+        if (!empty($gd_place_list_combined)) {
+            //get the ids from the gd_place_list_combined array
+            $gd_place_list_combined = array_map(function ($post) {
+                return $post['ID'];
+            }, $gd_place_list_combined);
+        }
+
+        // Set the post__in parameter for the main query
+        if (!empty($gd_place_list_combined)) {
+            $query->set('post__in', array_values($gd_place_list_combined));
+            $query->set('orderby', 'post__in');
+        } else {
+            $query->set('post__in', array(0));
+        }
     }
-}
+});
