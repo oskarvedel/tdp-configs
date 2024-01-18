641959ad2ffeb0001fc428822caf35615e0c4131
oskarvedel <oskar.vedel@gmail.com>
2024-01-18 15:49:07 +0100

    depotrum_page_query

--- tdp-configs-plugin.php
+++ tdp-configs-plugin.php
@@ -54,24 +54,21 @@ add_action('pre_get_posts', 'modify_archive_query', 1);
 
 //create custom query for "depotrum" page
 add_action('elementor/query/depotrum_page_query', function ($query) {
-    // Check if we are on the front end and if the main query is being modified
-    if (!is_admin() && $query->is_main_query()) {
-        $geolocation_id = 29783; //set geolocation id to Denmark
+    $geolocation_id = 17921; //set geolocation id to Denmark (tjekdepot.local)
+    $geolocation_id = 29783; //set geolocation id to Denmark
 
-        $gd_place_list_combined = get_post_meta($geolocation_id, 'archive_gd_place_list', false);
-        if (!empty($gd_place_list_combined)) {
-            //get the ids from the gd_place_list_combined array
-            $gd_place_list_combined = array_map(function ($post) {
-                return $post['ID'];
-            }, $gd_place_list_combined);
-        }
+    $gd_place_list_combined = get_post_meta($geolocation_id, 'archive_gd_place_list', false);
+    if (!empty($gd_place_list_combined)) {
+        // extract the ids from the gd_place_list_combined array
+        $gd_place_list_combined = array_map(function ($post) {
+            return $post['ID'];
+        }, $gd_place_list_combined);
 
-        // Set the post__in parameter for the main query
-        if (!empty($gd_place_list_combined)) {
-            $query->set('post__in', array_values($gd_place_list_combined));
-            $query->set('orderby', 'post__in');
-        } else {
-            $query->set('post__in', array(0));
-        }
+        $gd_place_list_combined = array_map('intval', $gd_place_list_combined);
     }
+
+    $query->set('post_type', 'gd_place');
+    $query->set('post__in', $gd_place_list_combined);
+    $query->set('orderby', 'post__in');
+    $query->set('posts_per_page', 10);
 });
